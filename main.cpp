2091c1676425d3de180ccdfd4ceabbef6601d2d0
Mateo Cervilla <mateo.cervilla@wazuh.com>
2024-01-22 02:24:13 -0300

    Add help arg

--- main.cpp
+++ main.cpp
@@ -1,40 +1,8 @@
+#include <iostream>
 #include "Game.h"
 
-static int checkArgs(int argc, char* argv[], int& iterations, bool& verbosity)
-{
-     // Loop through command line arguments
-    for (int i = 1; i < argc; i++) {
-        // Check if the argument starts with '-'
-        if (argv[i][0] == '-') {
-            // Parse the option and get the corresponding value
-            switch (argv[i][1]) {
-                case 'i':
-                    // Check if the next argument exists and is a valid integer
-                    if (i + 1 < argc) {
-                        iterations = std::atoi(argv[i + 1]);
-                        i++; // Skip the next argument, as it is the value for '-i'
-                    } else {
-                        std::cerr << "Error: Missing value for '-i' option." << std::endl;
-                        return 1; // Return error code
-                    }
-                    break;
-                case 'v':
-                    // Set verbosity true
-                    verbosity = true;
-                    break;
-                default:
-                    std::cerr << "Error: Unknown option '" << argv[i] << "'." << std::endl;
-                    return 1;
-            }
-        } else {
-            std::cerr << "Error: Invalid argument '" << argv[i] << "'." << std::endl;
-            return 1;
-        }
-    }
-
-    return 0;
-}
-
+static void checkArgs(int argc, char* argv[], int& iterations, bool& verbosity);
+void showHelp();
 
 int main(int argc, char* argv[]){
 
@@ -43,8 +11,7 @@ int main(int argc, char* argv[]){
     int iterations = 0;
     bool verbosity = false;
 
-    int ArgsErr = checkArgs(argc, argv, iterations, verbosity);
-    if (ArgsErr) return ArgsErr;
+    checkArgs(argc, argv, iterations, verbosity);
 
     // Init Roulette game
     Game rouletteSim;
@@ -63,4 +30,48 @@ int main(int argc, char* argv[]){
     rouletteSim.start();
 
     return 0;
+}
+
+static void checkArgs(int argc, char* argv[], int& iterations, bool& verbosity)
+{
+     // Loop through command line arguments
+    for (int i = 1; i < argc; i++) {
+        // Check if the argument starts with '-'
+        if (argv[i][0] == '-') {
+            // Parse the option and get the corresponding value
+            switch (argv[i][1]) {
+                case 'i':
+                    // Check if the next argument exists and is a valid integer
+                    if (i + 1 < argc) {
+                        iterations = std::atoi(argv[i + 1]);
+                        i++; // Skip the next argument, as it is the value for '-i'
+                    } else {
+                        std::cerr << "Error: Missing value for '-i' option." << std::endl;
+                        std::exit(1); // Return error code
+                    }
+                    break;
+                case 'v':
+                    // Set verbosity true
+                    verbosity = true;
+                    break;
+                case 'h':
+                    // Show help
+                    showHelp();
+                    std::exit(0);
+                default:
+                    std::cerr << "Error: Unknown option '" << argv[i] << "'." << std::endl;
+                    std::exit(1);
+            }
+        } else {
+            std::cerr << "Error: Invalid argument '" << argv[i] << "'." << std::endl;
+            std::exit(1);
+        }
+    }
+}
+
+void showHelp()
+{
+    std::cout << "Roulette Simulator" << std::endl << "------------------" << std::endl
+    << "Usage: ./roulette [options]" << std::endl << "-i <number> : Number of iterations (10.000 by default)"
+    << std::endl <<  "-v : Verbose mode" << std::endl;
 }
